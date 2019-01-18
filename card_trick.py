454b2d2275841ed5bf0c7634ad2f37ddc5218569
Cohen <cohen@dgrundy-MacBookPro.local>
2019-01-17 21:05:55 -0800

    adding pig_latin.py removing funny characters from card_trick.py

--- card_trick.py
+++ card_trick.py
@@ -12,75 +12,75 @@ if choice in spade:
 	print('spades. interesting choice. now, don\'t think of spades and randomly pick a direction. left   right')
 	dire=raw_input().lower()
 	if dire in left:
-		print('left, good choice, now mentaly select a card, but don\'t type it! J♢  Q♧  J♤  Q♡  K♧  K♢')
+		print('left, good choice, now mentaly select a card, but don\'t type it! Jd  Qc  Js  Qh  Kc  Kd')
 		finish=raw_input().lower()
 		if finish in done1:
 			print('think about your card and say it aloud twice')
 			finished=raw_input().lower()
 			if finished in done2:
-				print('we\'ve removed your card, J♡  Q♤  Q♢ K♤  K♡') 
+				print('we\'ve removed your card, Jh  Qs  Qd  Kd  Kh') 
 	if dire in right:
-		print('right, good choice, now mentaly select a card, but don\'t type it! J♢  Q♧  J♤  Q♡  K♧  K♢')
+		print('right, good choice, now mentaly select a card, but don\'t type it! Jd  Qc  Js  Qh  Kc  Kd')
 		finish=raw_input().lower()
 		if finish in done1:
 			print('think about your card and say it aloud twice')
 			finished=raw_input().lower()
 			if finished in done2:
-				print('we\'ve removed your card, J♡  Q♤  Q♢ K♤  K♡')
+				print('we\'ve removed your card, Jh  Qs  Qd  Ks  Kh')
 if choice in heart:
 	print('hearts. interesting choice. now, don\'t think of hearts and randomly pick a direction. left   right')
 	dire=raw_input().lower()
 	if dire in left:
-		print('left, good choice, now mentaly select a card, but don\'t   type it! J♢  Q♧  J♤  Q♡  K♧  K♢')
+		print('left, good choice, now mentaly select a card, but don\'t   type it! Jd  Qc  Js  Qh  Kc  Kd')
 		finish=raw_input().lower()
 		if finish in done1:
 			print('think about your card and say it aloud twice')
 			finished=raw_input().lower()
 			if finished in done2:
-				print('we\'ve removed your card, J♡  Q♤  Q♢ K♤  K♡') 
+				print('we\'ve removed your card, Jh  Qs  Qd  Kd  Kh') 
 	if dire in right:
-		print('right, good choice, now mentaly select a card, but don\'t type it! J♢  Q♧  J♤  Q♡  K♧  K♢')
+		print('right, good choice, now mentaly select a card, but don\'t type it! Jd  Qc  Js  Qh  Kc  Kd')
 		finish=raw_input().lower()
 		if finish in done1:
 			print('think about your card and say it aloud twice')
 			finished=raw_input().lower()
 			if finished in done2:
-				print('we\'ve removed your card, J♡  Q♤  Q♢ K♤  K♡')
+				print('we\'ve removed your card, Jh  Qs  Qd  Ks  Kh')
 
 if choice in club:
 	print('clubs. interesting choice. now, don\'t think of clubs and randomly pick a direction. left   right')
 	dire=raw_input().lower()
 	if dire in left:
-		print('left, good choice, now mentaly select a card, but don\'t   type it! J♢  Q♧  J♤  Q♡  K♧  K♢')
+		print('left, good choice, now mentaly select a card, but don\'t   type it! Jd  Qc  Js  Qh  Kc  Kd')
 		finish=raw_input().lower()
 		if finish in done1:
 			print('think about your card and say it aloud twice')
 			if choice in done2:
-				print('we\'ve removed your card, J♡  Q♤  Q♢ K♤  K♡') 
+				print('we\'ve removed your card, Jh  Qs  Qd  Kd  Kh') 
 	if dire in right:
-		print('right, good choice, now mentaly select a card, but don\'t itype it! J♢  Q♧  J♤  Q♡  K♧  K♢')
+		print('right, good choice, now mentaly select a card, but don\'t itype it! Jd  Qc  Js  Qh  Kc  Kd')
 		finish=raw_input().lower()
 		if finish in done1:
 			print('think about your card and say it aloud twice')
 			finished=raw_input().lower()
 			if finished in done2:
-				print('we\'ve removed your card, J♡  Q♤  Q♢ K♤  K♡')
+				print('we\'ve removed your card, Jh  Qs  Qd Kd  Kh')
 if choice in diamond:
 	print('diamonds. interesting choice. now, don\'t think of diamonds and randomly pick a direction. left   right')
 	dire=raw_input().lower()
 	if dire in left:
-		print('left, good choice, now mentaly select a card, but don\'t  type it! J♢  Q♧  J♤  Q♡  K♧  K♢')
+		print('left, good choice, now mentaly select a card, but don\'t  type it! Jd  Qc  Js  Qh  Kc  Kd')
 		finish=raw_input().lower()
 		if finish in done1:
 			print('think about your card and say it aloud twice')
 			finished=raw_input().lower()
 			if finished in done2:
-				print('we\'ve removed your card, J♡  Q♤  Q♢ K♤  K♡')
+				print('we\'ve removed your card, Jh  Qs  Qd Ks  Kh')
 	if dire in right:
-		print('right, good choice, now mentaly select a card, but don\'t type it! J♢  Q♧  J♤  Q♡  K♧  K♢')
+		print('right, good choice, now mentaly select a card, but don\'t type it! Jd  Qc  Js Qh  Kc  Kd')
 		finish=raw_input().lower()
 		if finish in done1:
 			print('think about your card and say it aloud twice')
 			finished=raw_input().lower()
 			if finished in done2:
-				print('we\'ve removed your card, J♡  Q♤  Q♢ K♤  K♡')
+				print('we\'ve removed your card, Jh  Qs  Qd  Ks  Kh')
---
+++ pig_latin.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+puncuation=[',', '.', '?', '!'
+vowels=['a','e','i','o','u']
+blends=['br','bl','cr','cl','ch','cz','dr','dj','dw','fr','fl','gr','gl','gn','gw','kn','kl','kr','ll','mn','pn','ps','pl','pr','pt','qu','sh','sl','sc','sk','sm','sn','sp','st','sv','sw','sch','scl','scr','spr','squ','str','tr','th','ts','wr','wh'
+]
+sentence=raw_input('pig latin translator: ')
+words=sentence.split()
+for word in words:
+	if word[0] in vowels: 
+		print(word+'way',end=' ')
+	elif word[0:3] in blends:
+		print(word[3:]+word[0:3]+'ay',end=' ')
+	elif word[0:2] in blends:
+		print(word[2:]+word[0:2]+'ay',end=' ')
+	else:
+		print(word[1:]+word[0]+'ay',end=' ')
+print('\n')	

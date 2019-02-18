2f722a7634f2e4450192721da323699a6e8c553d
cdgrundy <cohengrundy@gmail.com>
2019-02-18 12:25:15 -0800

    with translation, all working code!

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from card_trick import trick
 from pig_latin import translate
 while True:
     os.system('clear')
-    print('\n 1. pig latin translator \n\n 2. card trick \n\n 3. area/volume calculator')
-    choice=input('\nchoose one\n')
+    print('\n 1. traducteur de pig latin \n\n 2. tour de cartes \n\n 3. calculateur de l\'aire / volume')
+    choice=input('\nchoisi un\n')
     os.system('clear')
     try:
         if choice=='1':
